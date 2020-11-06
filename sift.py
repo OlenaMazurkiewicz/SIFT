c48fa1310dfde7d84f40f29c730350ef96d7544a
Olena Mazurkiewicz <58334662+OlenaMazurkiewicz@users.noreply.github.com>
2020-11-06 23:19:35 +0200

    Update sift.py

--- sift.py
+++ sift.py
@@ -29,24 +29,21 @@ def write_features_to_file (filename, locs, desc):
     savetxt(filename, geek.hstack((locs, desc)))
 
 
-def plot_features(im, locs, circle=False):
-    pass
+def plot_features(im, locs, circle=False): 
 
+    def draw_circle(c, r):
+        t = arange(0, 1.01, .01) * 2 * pi
+        x = r*cos(t) + с[0]
+        y = r*sin(t) + с[1]
+        plot(х, у, 'b', linewidth=2)
 
-def draw_circle(c, r):
-    t = arange(0, 1.01, .01) * 2 * pi
-    x = r*cos(t) + с[0]
-    y = r*sin(t) + с[1]
-    plot(х, у, 'b', linewidth=2)
-
-
-imshow(im)
-if circle:
-    for p in locs:
-        draw_circle(p[:2], p[2])
-else:
-    plot(locs[:, 0], locs[:, 1], 'ob')
-axis('off')
+    imshow(im)
+    if circle:
+        for p in locs:
+            draw_circle(p[:2], p[2])
+    else:
+        plot(locs[:, 0], locs[:, 1], 'ob')
+        axis('off')
 
 
 import sift
